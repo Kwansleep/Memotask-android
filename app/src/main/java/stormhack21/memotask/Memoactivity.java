d7b738fe79cba89ca4f0a3554bf55ca111240c99
Warren Han <warren@home.com>
2021-02-20 13:31:37 -0800

    memo

---
+++ app/src/main/java/stormhack21/memotask/Memoactivity.java
@@ -0,0 +1,16 @@
+package stormhack21.memotask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Memoactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memoactivity);
+
+
+    }
+}
